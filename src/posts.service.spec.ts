73b3250ea5e3a357adb8c1ed5f977194091e4eed
Avet90 <avet.sarkisian90@icloud.com>
2024-03-16 12:21:47 +0300

    first

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,33 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
+  it("should add a new post", () => {
+    //arrange
+    const textPost = "Some pre-existing post";
+    const arrayPosts = Object.values(post);
+    // act
+    const findPost = arrayPosts.find((item) => item === textPost);
+    //assert
+    expect(findPost).toEqual(textPost);
   });
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+  it("should find a post", () => {
+    //arrange
+    const idPost = "2";
+    //act
+
+    //assert
+    expect(postsService.find(idPost)).toEqual(idPost);
   });
 });
